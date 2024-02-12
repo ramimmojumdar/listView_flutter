49100f49afbdbd4b66dbecc77e5c733297ae934b
ramimmojumdar <expertramim@gmial.com>
2024-02-12 23:34:24 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/listview/list_view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listview.list_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  final String title = "List View App";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          backgroundColor: Colors.green,
+        ),
+        body: HomeActivity(),
+      ),
+    );
+  }
+}
+
+class HomeActivity extends StatefulWidget {
+  const HomeActivity({super.key});
+
+  @override
+  State<HomeActivity> createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+
+      children: [
+
+        ListTile(
+          title: Text("Item 1"),
+          tileColor: Colors.amber,
+          leading: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, // Set your desired color
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Icon(Icons.star),
+
+          ),
+        ),
+
+        ListTile(
+          title: Text("Item 1"),
+          tileColor: Colors.amber,
+          leading: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, // Set your desired color
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Icon(Icons.star),
+
+          ),
+        ),
+
+        ListTile(
+          title: Text("Item 1"),
+          tileColor: Colors.amber,
+          leading: Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, // Set your desired color
+                borderRadius: BorderRadius.circular(5.0)
+            ),
+            child: Icon(Icons.star),
+
+          ),
+        )
+
+      ],
+
+    );
+  }
+}
+
